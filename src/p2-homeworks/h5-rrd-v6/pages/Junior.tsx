a68ae920b86694cf609d40f9679630694c14f14f
Andrewwxo <andrewwxo@gmail.com>
2022-08-22 18:30:22 +0300

    HW8

--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HW6 from '../../h6/HW6';
 import HW7 from '../../h7/HW7';
+import HW8 from '../../h8/HW8';
 
 function Junior() {
     return (
@@ -8,6 +9,7 @@ function Junior() {
             Empty page
             Junior
             <HW7/>
+            <HW8/>
         </div>
     )
 }
