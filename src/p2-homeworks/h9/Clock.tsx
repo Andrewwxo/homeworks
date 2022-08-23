43fff28b7d62db22f911b9b01466c0253dd1e758
Andrewwxo <andrewwxo@gmail.com>
2022-08-23 15:38:28 +0300

    HW9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {brotliCompress} from 'zlib';
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
@@ -8,24 +9,28 @@ function Clock() {
 
     const stop = () => {
         // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
             // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
         // show
+        setShow(true)
     }
     const onMouseLeave = () => {
         // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date?.toLocaleTimeString() || <br/> // fix with date
+    const stringDate = date?.toLocaleDateString() || <br/>// fix with date
 
     return (
         <div>
@@ -36,11 +41,13 @@ function Clock() {
                 {stringTime}
             </div>
 
-            {show && (
+            {show ?  (
                 <div>
                     {stringDate}
                 </div>
-            )}
+            ) : (
+                <br/>
+                )}
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
